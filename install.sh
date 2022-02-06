03033f5d694448c08f4ef3d9441727b5ce11fb84
Lrapava <46052668+Lrapava@users.noreply.github.com>
2022-02-07 00:14:12 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,37 @@
-echo Verifying the boot mode:
-sleep 2s
-ls /sys/firmware/efi/efivars
-sleep 5s
+in_arch() {
+	echo '
+		ln -sf /usr/share/zoneinfo/Asia/Tbilisi /etc/localtime
+		hwclock --systohc
+		echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+		locale-gen
+		echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+		echo "ravenholm" >> /etc/hostname
+		echo "127.0.0.1	localhost" >> /etc/hosts
+		echo "::1		localhost" >> /etc/hosts
+		echo "127.0.1.1	ravenholm.localdomain	ravenholm" >> /etc/hosts
+		passwd
+		grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+		grub-mkconfig -o /boot/grub/grub.cfg
+		exit
+	'
+}
 
 timedatectl set-ntp true
 
-cgdisk /dev/sda
-mkfs.fat -F 32 /dev/sda1
-mkfs.btrfs /dev/sda2
-mkfs.btrfs /dev/sda3
-mount /dev/sda2 /mnt
-mount /dev/sda3 /mnt/home
-mount /dev/sda1 /mnt/boot
+fdisk /dev/sda
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
 
-pacstrap /mnt base linux-lts linux-firmware vim nano micro 
+mount /dev/sda2 /mnt
+mkdir -p /mnt/boot /mnt/home
+mount /dev/sda1 /mnt/boot
+mount /dev/sda3 /mnt/home
+
+pacstrap /mnt base linux linux-firmware micro nano vim grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-headers
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/Asia/Tbilisi /etc/localtime
-hwclock --systohc
-micro /etc/locale.gen
-locale-gen
-micro /etc/locale.conf
-micro /etc/hostname
-passwd
-pacman -S grub
-exit
-umount -R /mnt
+
+in_arch | arch-chroot /mnt
+
+umount -a
 reboot
